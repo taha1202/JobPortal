530baf24cc87d2e335addb7b0a9b2a69b97425b8
unknown <tahaghulam10@gmail.com>
2024-12-02 02:22:08 +0500

    Corrected port for databse deployement

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ const { GetCategory } = require('./controllers/jobSearchController');
 const app = express();
 
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 10000; 
 
 app.use(express.json());
 app.use(cors());
