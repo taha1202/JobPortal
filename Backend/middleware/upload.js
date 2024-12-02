517da646c02af2ef6648d576fead4c9a1b64afa9
unknown <tahaghulam10@gmail.com>
2024-12-02 20:42:42 +0500

    Install Packages For Uploading

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -1,31 +1,16 @@
 const multer = require('multer');
-const multerS3 = require('multer-s3');
-const AWS = require('aws-sdk');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('../config/cloudinaryConfig');  
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
 
-const uploadS3 = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: 'your-s3-bucket-name',
-    key: function (req, file, cb) {
-      cb(null, `${Date.now()}-${file.originalname}`); // File name
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB
-  fileFilter: (req, file, cb) => {
-    const allowedExtensions = ['.pdf', '.docx', '.jpg', '.jpeg', '.png'];
-    const ext = path.extname(file.originalname).toLowerCase();
-    if (allowedExtensions.includes(ext)) {
-      cb(null, true);
-    } else {
-      cb(new Error('Invalid file type'), false);
-    }
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'uploads', 
+    allowed_formats: ['jpg', 'jpeg', 'png', 'pdf'],  
   },
 });
 
-module.exports = uploadS3;
+const upload = multer({ storage: storage });
+
+module.exports = upload;
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const { signup, login} = require('../controllers/userController');
 const router = express.Router();
-const { uploadS3 } = require('../middleware/upload');
 
 
-router.post('/upload-resume', uploadS3.single('U_resume'), (req, res) => {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded' });
-    }
-    res.status(200).json({
-      message: 'Resume uploaded successfully',
-      url: req.file.location, 
-    });
+router.post('/upload-resume', upload.single('U_resume'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  res.status(200).json({
+    message: 'Resume uploaded successfully',
+    url: req.file.path,  
   });
-  
+});
 
-  router.post('/upload-image', uploadS3.single('picture'), (req, res) => {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded' });
-    }
-    res.status(200).json({
-      message: 'Image uploaded successfully',
-      url: req.file.location, 
-    });
+router.post('/upload-image', upload.single('picture'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  res.status(200).json({
+    message: 'Image uploaded successfully',
+    url: req.file.path,  
   });
+});
 
 router.post('/signup', signup);
 router.post('/login', login);
