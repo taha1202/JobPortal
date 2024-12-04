cbea9bdc4196db9783305c39646e4f0c8f196be4
unknown <tahaghulam10@gmail.com>
2024-12-04 19:09:34 +0500

    Installed Some Packages

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const app = express();
 
 const pool = mysql.createPool({
